68f6c2d0b666131405db974e9c2e6d11bd5dee1f
Euph0nia <bigsasha@lenta.ru>
2024-04-07 14:21:27 +0300

    #3 commit

--- 1lab_IntSys/Form1.cs
+++ 1lab_IntSys/Form1.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Windows.Forms;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace _1lab_IntSys
@@ -11,16 +12,16 @@ namespace _1lab_IntSys
         private Stopwatch stopwatch;
         private int clickCount = 0;
         private float avgTime;
+        //private bool bRestart = false;
 
-        private List<long> reactionTimes = new List<long>();
-        private List<Button> up_buttons = new List<Button>();
-        private List<Button> r_buttons = new List<Button>();
+        //private List<long> reactionTimes = new List<long>();
         private List<Button> all_buttons = new List<Button>();
 
 
         public Keyboard()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
             SetupKey();
             textBox.AppendText($"Вас приветствует тест определяющий реакцию пользователя на события от клавиатуры.\r\n" +
                                 $"В тесте представлено 10 событий, после чего подводится итог. \r\n");
@@ -31,7 +32,6 @@ namespace _1lab_IntSys
                     all_buttons.Add((Button)Controls[i]);
                 }
             }
-
             SetupTimer();
         }
 
@@ -51,25 +51,36 @@ namespace _1lab_IntSys
 
         private void stopTest()
         {
-            textBox.AppendText($"\n\nТест завершен, среднее время реакции {avgTime}\r\n");
-            
+            textBox.AppendText($"\n\nТест завершен, среднее время реакции {avgTime / clickCount} мс.\r\n" +
+                                $"Если хотите пройти еще раз, нажмите Enter\r\n");
+            for (int i = 0; i < 20; i++)
+            {
+                all_buttons[i].BackColor = DefaultBackColor;
+            }
+            _timer.Stop();
+            timer.Stop();
+            stopwatch.Stop();
+            //bRestart = true;
         }
 
         private void Keyboard_KeyDown(object sender, KeyEventArgs e)
         {
-            if ((int)e.KeyCode == keyCode)
+            if (clickCount == 10) { stopTest(); }
+            //else if (e.KeyCode == Keys.Enter /*&& bRestart == true*/)
+            //{
+            //    Application.Restart();
+            //}
+            else if ((int)e.KeyCode == keyCode)
             {
                 if (stopwatch != null && stopwatch.IsRunning)
                 {
                     stopwatch.Stop();
                     button.BackColor = DefaultBackColor;
-
-                    reactionTimes.Add(stopwatch.ElapsedMilliseconds);
-                    avgTime =+ stopwatch.ElapsedMilliseconds;
+                    //reactionTimes.Add(stopwatch.ElapsedMilliseconds);
                     textBox.AppendText($"Время нажатия: {stopwatch.ElapsedMilliseconds} мс. \r\n");
                     clickCount++;
+                    avgTime += stopwatch.ElapsedMilliseconds;
                 }
-                if (clickCount == 10) { stopTest(); }
                 _timer.Interval = new Random().Next(2000, 3000);
                 _timer.Start();
             }
@@ -78,12 +89,14 @@ namespace _1lab_IntSys
         private void timer_Tick(object sender, EventArgs e)
         {
             timer.Stop();
+            if(button != null) { button.BackColor = DefaultBackColor; }
             Random random = new Random();
 
             button = all_buttons[random.Next(all_buttons.Count)];
             keyCode = 48 + Int32.Parse(button.Text);
             button.BackColor = Color.BlueViolet;
-            
+
+
 
             stopwatch = Stopwatch.StartNew();
         }
